f59db2f4907c7c815b2b5ff8c5f6b71fa8f38624
James Y <jyab0617@gmail.com>
2024-06-26 13:05:03 -0400

    add query to create users

--- server/index.js
+++ server/index.js
@@ -10,6 +10,18 @@ app.use(express.json()); // gives access to the request body
 
 // ROUTES
 
+// create user
+app.post("/create_user", async (req, res) => {
+    try {
+        const { uname, password } = req.body;
+        const newUser = await pool.query(
+            `INSERT INTO users (uname, hashedpw) VALUES(${uname}, ${password});`
+        )
+        console.log(`The user ${uname} has been created`);
+    } catch (error) {
+        console.log(error.message);
+    }
+});
 
 app.listen(5000, () => {
     console.log("server has started on port 5000");
