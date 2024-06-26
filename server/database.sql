a9eaf3416710258a559acd3be4a4a1c7cc41cfad
James Y <jyab0617@gmail.com>
2024-06-26 12:06:12 -0400

    modify unique id's to type char

--- server/database.sql
+++ server/database.sql
@@ -1,22 +1,22 @@
 -- create database if not exists hackhero;
 -- use hackhero;
 CREATE TABLE if not exists Users (
-    UID INTEGER PRIMARY KEY,
+    UID CHAR(36) PRIMARY KEY,
     UName VARCHAR(16),
     HashedPW VARCHAR(64)
 );
 CREATE TABLE if not exists Admins (
-    UID INTEGER,
+    UID CHAR(36),
     CONSTRAINT FK_Admin FOREIGN KEY (UID) REFERENCES Users (UID) ON DELETE CASCADE,
     CONSTRAINT PK_Admin PRIMARY KEY (UID)
 );
 CREATE TABLE if not exists Player (
-    UID INTEGER NOT NULL,
+    UID CHAR(36) NOT NULL,
     CONSTRAINT FK_Player FOREIGN KEY (UID) REFERENCES Users (UID) ON DELETE CASCADE,
     CONSTRAINT PK_Player PRIMARY KEY (UID)
 );
 CREATE TABLE if not exists Problems (
-    PID INTEGER PRIMARY KEY,
+    PID CHAR(36) PRIMARY KEY,
     ProblemLink VARCHAR(250),
     Difficulty VARCHAR(8)
 );
@@ -24,23 +24,23 @@ CREATE TABLE if not exists Topics (Type VARCHAR(20) PRIMARY KEY);
 
 -- aggregation (RelatedTo)
 CREATE TABLE if not exists Levels (
-    PID INTEGER REFERENCES Problems,
+    PID CHAR(36) REFERENCES Problems,
     Type VARCHAR(20) REFERENCES Topics,
     CONSTRAINT PK_Quests PRIMARY KEY (PID, Type)
 );
 CREATE TABLE if not exists Quests (
-    QID INTEGER PRIMARY KEY,
+    QID CHAR(36) PRIMARY KEY,
     UID INTEGER NOT NULL REFERENCES Admins (UID) ON DELETE CASCADE
 );
 CREATE TABLE if not exists Features (
-    QID INTEGER REFERENCES Quests,
-    PID INTEGER REFERENCES Problems,
+    QID CHAR(36) REFERENCES Quests,
+    PID CHAR(36) REFERENCES Problems,
     Type VARCHAR(20) REFERENCES Topics,
     CONSTRAINT PK_Features PRIMARY KEY (QID, PID, Type)
 );
 CREATE TABLE if not exists CompletedBy (
-    UID INTEGER REFERENCES Player,
-    PID INTEGER REFERENCES Problems,
+    UID CHAR(36) REFERENCES Player,
+    PID CHAR(36) REFERENCES Problems,
     Type VARCHAR(20) REFERENCES Topics,
     Date DATE NOT NULL DEFAULT (current_date),
     CONSTRAINT PK_CompletedBy PRIMARY KEY (UID, PID, Type)
